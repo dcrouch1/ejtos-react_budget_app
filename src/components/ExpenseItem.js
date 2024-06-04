73c74f9a94c751a6bfe3084e446cac7f7de7bdee
dcrouch1 <dcrouch@ca.ibm.com>
2024-06-04 17:04:51 -0400

    Add updates to Git

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -22,7 +22,17 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
+    }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
 
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
     }
 
     return (
@@ -30,6 +40,7 @@ const ExpenseItem = (props) => {
         <td>{props.name}</td>
         <td>£{props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
